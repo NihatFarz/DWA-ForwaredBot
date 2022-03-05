2ad715534a33ba0a8bacf2fd6c6c6c923dd1429d
Nihat 「 FARZ 」 <99778063+NihatFarz@users.noreply.github.com>
2022-03-06 01:41:27 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,3 @@
-#    Copyright (c) 2021 Ayush
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
-
 from telethon import TelegramClient, events
 from decouple import config
 import logging
@@ -18,9 +5,9 @@ from telethon.sessions import StringSession
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-print("Starting...")
+print("Başladılır...")
 
-# Basics
+# Əsaslar
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 SESSION = config("SESSION")
@@ -31,22 +18,22 @@ FROM = [int(i) for i in FROM_.split()]
 TO = [int(i) for i in TO_.split()]
 
 try:
-    BotzHubUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
-    BotzHubUser.start()
+    Farz = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
+    Farz.start()
 except Exception as ap:
     print(f"ERROR - {ap}")
     exit(1)
 
-@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
+@Farz.on(events.NewMessage(incoming=True, chats=FROM))
 async def sender_bH(event):
     for i in TO:
         try:
-            await BotzHubUser.send_message(
+            await Farz.send_message(
                 i,
                 event.message
             )
         except Exception as e:
             print(e)
 
-print("Bot has started.")
-BotzHubUser.run_until_disconnected()
+print("Bot İşləkdir.")
+Farz.run_until_disconnected()
